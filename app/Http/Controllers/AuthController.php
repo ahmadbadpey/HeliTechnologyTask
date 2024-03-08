339f79acfcbbda954e0868845cbc3f24e225ea11
AhmadBadpey <ahmadbadpey@Ahmad.local>
2024-03-08 11:53:00 +0330

    Initilizing Project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = auth()->user()->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+}
